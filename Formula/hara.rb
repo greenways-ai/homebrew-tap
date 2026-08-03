5b47e15dbc1b8c150a25e7613d12f39f2e0cdb6c
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-08-04 06:12:23 +1000

    build Hara from the checksummed release archive

--- Formula/hara.rb
+++ Formula/hara.rb
@@ -1,18 +1,15 @@
 class Hara < Formula
   desc "Symbolic programming language and portable AST kernel"
   homepage "https://www.hara-lang.org"
-  url "https://github.com/hara-lang/hara.git",
-      tag:      "v0.1.0",
-      revision: "5ae3449e461274323318ceb33131111c53210835"
+  url "https://github.com/hara-lang/hara/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "56b763d4c39d4e8ecfb0cb2eeca80fcebae1b3ab3a5299862c2619bc7bc5317f"
   license "EPL-2.0"
   head "https://github.com/hara-lang/hara.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--locked", "--release",
-           "--manifest-path", "rust/Cargo.toml", "--bin", "hara"
-    bin.install "rust/target/release/hara"
+    system "cargo", "install", *std_cargo_args(path: "rust")
   end
 
   test do
